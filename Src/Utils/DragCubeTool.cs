6c6e1c57604b1082ee5cc389d0d9facbb94c5f20
Alvin Meng <alvin.q.meng@gmail.com>
2024-05-19 14:55:01 -0400

    vendor a copy of DragCubeTool

---
+++ Src/Utils/DragCubeTool.cs
@@ -0,0 +1,64 @@
+﻿using System.Linq;
+using UnityEngine;
+
+namespace ProceduralTools;
+
+public class DragCubeTool : MonoBehaviour
+{
+    public Part part;
+
+    public static DragCubeTool UpdateDragCubes(Part p, bool immediate = false)
+    {
+        var tool = p.GetComponent<DragCubeTool>();
+        if (tool == null)
+        {
+            tool = p.gameObject.AddComponent<DragCubeTool>();
+            tool.part = p;
+            if (immediate && tool.Ready())
+                tool.UpdateCubes();
+        }
+
+        return tool;
+    }
+
+    public void FixedUpdate()
+    {
+        if (Ready())
+            UpdateCubes();
+    }
+
+    public bool Ready()
+    {
+        if (HighLogic.LoadedSceneIsFlight)
+            return FlightGlobals.ready; //&& !part.packed && part.vessel.loaded;
+        if (HighLogic.LoadedSceneIsEditor)
+            return part.localRoot == EditorLogic.RootPart &&
+                   part.gameObject.layer != LayerMask.NameToLayer("TransparentFX");
+        return true;
+    }
+
+    private void UpdateCubes()
+    {
+        if (FARInstalled)
+            part.SendMessage("GeometryPartModuleRebuildMeshData");
+        var dragCube = DragCubeSystem.Instance.RenderProceduralDragCube(part);
+        part.DragCubes.ClearCubes();
+        part.DragCubes.Cubes.Add(dragCube);
+        part.DragCubes.ResetCubeWeights();
+        part.DragCubes.ForceUpdate(true, true, false);
+        part.DragCubes.SetDragWeights();
+        Destroy(this);
+    }
+
+    private static bool? _FARInstalled;
+
+    public static bool FARInstalled
+    {
+        get
+        {
+            _FARInstalled ??= AssemblyLoader.loadedAssemblies.Any(a =>
+                a.assembly.GetName().Name == "FerramAerospaceResearch");
+            return _FARInstalled.Value;
+        }
+    }
+}
--- Src/Utils/TransformUtils.cs
+++ Src/Utils/TransformUtils.cs
@@ -7,7 +7,7 @@ public class TransformUtils
 {
     public static Transform GetOrCreateAnchorTransform(Part part, string name)
     {
-        if (part.transform.FindDeepChild(name) is { } existingAnchor) return existingAnchor;
+        if (part.transform.FindDeepChild(name) is Transform existingAnchor) return existingAnchor;
         var newAnchor = new GameObject(name).transform;
         newAnchor.NestToParent(part.transform.FindDeepChild("model"));
         return newAnchor;
