4461466539482452b733ff63c7392d3194da0ee5
Alvin Meng <alvin.q.meng@gmail.com>
2024-05-19 13:25:44 -0400

    fix: clear position when generating anchor

--- Src/Utils/TransformUtils.cs
+++ Src/Utils/TransformUtils.cs
@@ -9,7 +9,7 @@ public class TransformUtils
     {
         if (part.transform.FindDeepChild(name) is { } existingAnchor) return existingAnchor;
         var newAnchor = new GameObject(name).transform;
-        newAnchor.SetParent(part.transform.FindDeepChild("model"));
+        newAnchor.NestToParent(part.transform.FindDeepChild("model"));
         return newAnchor;
     }
 }
