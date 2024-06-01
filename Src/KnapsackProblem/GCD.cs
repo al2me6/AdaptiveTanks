9300c5d8e53935641980b5dfe2f18d9773b8fafb
Alvin Meng <alvin.q.meng@gmail.com>
2024-06-01 17:48:32 -0400

    half of a knapsack solver

---
+++ Src/KnapsackProblem/GCD.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace KnapsackProblem;
+
+public static class Divisors
+{
+    public static float GCD(float a, float b)
+    {
+        while (b > float.Epsilon)
+        {
+            (a, b) = (b, a % b);
+        }
+
+        return a;
+    }
+
+    public static float GCD(this IEnumerable<float> vals) => vals.Aggregate(GCD);
+}
---
+++ Src/KnapsackProblem/KnapsackProblem.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace KnapsackProblem;
+
+public interface IKnapsackItem
+{
+    public float Weight();
+}
+
+public static class UnboundedKnapsack
+{
+    public static List<T> SolveFloatApproximateAsDP<T>(T[] items, float maxWeight,
+        Func<T, float> appraiser, float tolerance)
+        where T : IKnapsackItem
+    {
+        var values = items.Select(appraiser).ToArray();
+
+        var itemWeightGCD = items.Select(item => item.Weight()).GCD();
+        var discretizationFactor = Math.Max(tolerance, itemWeightGCD);
+
+        var rescaledWeights = items
+            .Select(item => item.Weight())
+            .Select(weight => (int)Math.Round(weight / discretizationFactor))
+            .ToArray();
+        var rescaledMaxWeight = (int)Math.Round(maxWeight / discretizationFactor);
+
+        return SolveDP(items, rescaledWeights, values, rescaledMaxWeight);
+    }
+
+    public static List<T> SolveDP<T>(T[] items, int[] weights, float[] values, int maxWeight)
+    {
+        var dp = new float[maxWeight + 1];
+
+        for (var i = 0; i <= maxWeight; ++i)
+        {
+            for (var j = 0; j < items.Length; ++j)
+            {
+                if (weights[j] <= i)
+                {
+                    dp[i] = Math.Max(dp[i], dp[i - weights[j]] + values[j]);
+                }
+            }
+        }
+
+        return new List<T>();
+    }
+}
