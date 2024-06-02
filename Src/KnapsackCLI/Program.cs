20e0b82ec03946f8108a9147e769e7c3e44c20fe
StonesmileGit <62522391+StonesmileGit@users.noreply.github.com>
2024-06-02 18:57:16 +0200

    Fix reference path

--- Src/KnapsackCLI/Program.cs
+++ Src/KnapsackCLI/Program.cs
@@ -1,10 +1,10 @@
 ﻿using KnapsackProblem;
 
 var items = new MyItem[3];
-items[0] = new MyItem(1);
-items[1] = new MyItem(2);
-items[2] = new MyItem(3);
-var res = UnboundedKnapsack.SolveFloatApproximateAsDP(items, 10, (a) => a.Weight(), 1);
+items[0] = new MyItem(1.9f, 1);
+items[1] = new MyItem(2.9f, 2);
+items[2] = new MyItem(3.9f, 3);
+var res = UnboundedKnapsack.SolveFloatApproximateAsDP(items, 11, (a) => a.value, 1);
 foreach (var item in res)
 {
     Console.WriteLine(item.weight);
@@ -13,10 +13,12 @@ foreach (var item in res)
 class MyItem : IKnapsackItem
 {
     public float weight;
+    public float value;
 
-    public MyItem(float weight)
+    public MyItem(float weight, float value)
     {
         this.weight = weight;
+        this.value = value;
     }
 
     public float Weight()
