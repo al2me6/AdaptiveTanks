5f0480453684ede249c5f778b90d5bfe6105e8d7
Alvin Meng <alvin.q.meng@gmail.com>
2024-06-30 23:10:25 -0400

    fix: display part cost & prop volume correctly in the tooltip

--- Src/AdaptiveTanks/ModuleAdaptiveTankBase.Lifecycle.cs
+++ Src/AdaptiveTanks/ModuleAdaptiveTankBase.Lifecycle.cs
@@ -9,9 +9,20 @@ public abstract partial class ModuleAdaptiveTankBase : PartModule
         if (IsLoadingPrefab) LoadCustomDataFromConfig(node);
     }
 
+    // This gets called on a copy of the prefab, not the prefab itself.
     public override void OnIconCreate() => InitializeConfigurationAndModel();
 
+    // This is for some reason necessary when loading a ship. Otherwise, Unity crashes.
     public override void OnInitialize() => InitializeConfigurationAndModel();
+
+    public override string GetInfo()
+    {
+        // This is one of the few methods that gets called on the prefab PM. Initialize here so
+        // the part info window has cost/propellant info.
+        if (IsLoadingPrefab) InitializeConfigurationAndModel();
+
+        return base.GetInfo();
+    }
 
     public override void OnStart(StartState state)
     {
